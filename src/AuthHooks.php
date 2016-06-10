64f49cab946a24780cf42481277259b505a80493
Robin Radic <robin@radic.nl>
2016-06-11 00:24:32 +0200

    improvements

--- src/AuthHooks.php
+++ src/AuthHooks.php
@@ -3,8 +3,11 @@ namespace Codex\Addon\Auth;
 
 use Codex\Addons\Annotations\Hook;
 use Codex\Contracts\Codex;
+use Codex\Contracts\Projects\Projects;
 use Codex\Documents\Document;
 use Codex\Http\Controllers\CodexController;
+use Codex\Menus\Menu;
+use Codex\Menus\Node;
 use Codex\Projects\Project;
 
 class AuthHooks
@@ -22,16 +25,93 @@ class AuthHooks
      */
     public function controllerDocument(CodexController $controller, Document $document, Codex $codex, Project $project)
     {
-        if($project->config('auth.enabled', false) !== true){
+        if ( false === $this->hasEnabledAuth($project) ) {
             return;
         }
-        if($codex->auth->hasAccess($project) === false){
+        if ( $codex->auth->hasAccess($project) === false ) {
             return $codex->error(
                 config('codex-auth.error.title'),
                 config('codex-auth.error.text'),
                 403,
                 2
             );
+        }
+    }
+
+
+    /**
+     * @Hook("projects:resolved:node")
+     * @param \Codex\Contracts\Projects\Projects $projects
+     * @param \Codex\Projects\Project            $project
+     * @param \Codex\Menus\Node                  $node
+     *
+     * @throws \Codex\Exception\CodexException
+     */
+    public function projectsResolvedNode(Projects $projects, Project $project, Node $node)
+    {
+        if ( false === $this->hasEnabledAuth($project) ) {
+            return;
+        }
+        if ( false === $project->hasAccess() ) {
+            $node->setMeta('hidden', true);
+        }
+    }
+
+    /**
+     * @Hook("constructed")
+     * @param \Codex\Contracts\Codex $codex
+     */
+    public function codexConstructed(Codex $codex)
+    {
+        $codex->extend('auth', CodexAuth::class);
+    }
+
+    /**
+     * @Hook("project:constructed")
+     * @param \Codex\Projects\Project $project
+     */
+    public function projectConstructed(Project $project)
+    {
+        $project->extend('hasEnabledAuth', function () use ($project) {
+            return $project->config('auth.enabled', false) === true;
+        });
+        $project->extend('hasAccess', function () use ($project) {
+            if ( false === $project->hasEnabledAuth() ) {
+                return true;
+            }
+            if ( $project->getCodex()->auth->hasAccess($project) ) {
+                return true;
+            }
+            return false;
+        });
+    }
+
+
+    /**
+     * @Hook("menu:render")
+     * @param \Codex\Menus\Menu $menu
+     * @param \Codex\Menus\Node $root
+     * @param                   $sorter
+     */
+    public function menuRender(Menu $menu, Node $root, $sorter)
+    {
+        if ( $menu->getId() === 'auth' && $menu->attr('resolved', false) !== true ) {
+            $auth = codex()->auth;
+
+            foreach ( $auth->getDrivers() as $driver ) {
+                if ( $auth->isLoggedIn($driver) === false ) {
+                    $node = $menu->add('auth-login-' . $driver, 'Login to ' . ucfirst($driver));
+                    $node->setAttribute('href', route('codex.auth.login', $driver));
+                }
+            }
+
+            foreach ( $auth->getDrivers() as $driver ) {
+                if ( $auth->isLoggedIn($driver) === true ) {
+                    $node = $menu->add('auth-logout-' . $driver, 'Logout from ' . ucfirst($driver));
+                    $node->setAttribute('href', route('codex.auth.logout', $driver));
+                }
+            }
+            $menu->setAttribute('resolved', true);
         }
     }
 
--- src/AuthServiceProvider.php
+++ src/AuthServiceProvider.php
@@ -1,9 +1,6 @@
 <?php
 namespace Codex\Addon\Auth;
 
-use Codex\Contracts\Codex;
-use Codex\Contracts\Projects\Projects;
-use Codex\Projects\Project;
 use Codex\Traits\CodexProviderTrait;
 use Sebwite\Support\ServiceProvider;
 
@@ -20,7 +17,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $assetDirs = [ 'assets' => 'codex-auth' ];
 
     protected $shared = [
-        'codex.auth' => CodexAuth::class
+        'codex.auth' => CodexAuth::class,
     ];
 
     protected $providers = [
@@ -33,77 +30,30 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $app = parent::boot();
-
         $this->codex()
             ->theme
             ->addStylesheet('auth', 'vendor/codex-auth/styles/auth')
             ->addBodyClass('codex-auth');
-
-        #$this->bootMenu();
         $this->codex()->pushToStack('header', $this->codexView('auth.header-auth-menu'));
-
         return $app;
     }
 
     protected function bootMenu()
     {
-
         $menu = $this->codex()->menus->add('auth');
         $menu->setAttribute('title', 'Auth');
         $menu->setAttribute('subtitle', 'Auth');
         $menu->setView($this->codex()->view('menus.auth'));
-        $this->codexHook('menu:render', function(\Codex\Menus\Menu $menu, \Codex\Menus\Node $root, $sorter){
-            if($menu->getId() === 'auth' && $menu->attr('resolved', false) !== true ){
-                $auth = codex()->auth;
-
-                foreach($auth->getDrivers() as $driver)
-                {
-                    if ( $auth->isLoggedIn($driver) === false )
-                    {
-                        $node = $menu->add('auth-login-' . $driver, 'Login to ' . ucfirst($driver));
-                        $node->setAttribute('href', route('codex.auth.login', $driver));
-                    }
-                }
-
-                foreach($auth->getDrivers() as $driver)
-                {
-                    if ( $auth->isLoggedIn($driver) === true )
-                    {
-                        $node = $menu->add('auth-logout-' . $driver, 'Logout from ' . ucfirst($driver));
-                        $node->setAttribute('href', route('codex.auth.logout', $driver));
-                    }
-                }
-                $menu->setAttribute('resolved', true);
-            }
-        });
         $this->codex()->pushToStack('header', $this->codexView('auth.header-auth-menu'));
-
     }
 
     public function register()
     {
         $app = parent::register();
         $this->codexIgnoreRoute('auth');
-        $this->registerCodexAuthExtension();
-
         $this->codexView('menus.auth', 'codex::menus.header-dropdown');
         $this->codexView('auth.header-auth-menu', 'codex-auth::header-auth-menu');
-
-        $this->codexHook('project:make', function(Projects $projects, Project $project){
-            if($project->config('auth.enabled', false) === true && $project->getCodex()->auth->hasAccess($project) === false){
-                return false;
-            }
-        });
-
         return $app;
     }
 
-
-    protected function registerCodexAuthExtension()
-    {
-        $this->codexHook('constructed', function (Codex $codex)
-        {
-            $codex->extend('auth', CodexAuth::class);
-        });
-    }
 }
